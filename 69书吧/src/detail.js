cb4e859296bc6fab8003cf818b5306aa2eb7c803
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-07-17 09:03:41 +0700

    fix des

--- 69书吧/src/detail.js
+++ 69书吧/src/detail.js
@@ -7,12 +7,13 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html('gbk');
-
+		
+		doc.select('div#jianjie-popup.jianjie-popup').remove()
         return Response.success({
             name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
             cover: $.Q(doc, 'div.bookimg2 > img').attr('src'),
             author: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
-            description: $.Q(doc, 'div.content > p', {remove: ['div#jianjie-popup.jianjie-popup']}).html(),
+            description: $.Q(doc, 'div.content > p').html(),
             detail: $.QA(doc, 'div.booknav2 p', {m: x => x.text(), j: '<br>'}),
             host: BASE_URL
         })
