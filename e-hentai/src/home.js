712dc11b237f716baabb907259c30cd70da9f00f
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-07-22 13:39:42 +0700

    update e-hentai

--- e-hentai/src/home.js
+++ e-hentai/src/home.js
@@ -1,6 +1,10 @@
 function execute() {
     return Response.success([
         {title: "Front Page", input: "https://e-hentai.org", script: "gen.js"},
-        {title: "Popular", input: "https://e-hentai.org/popular", script: "gen.js"}
+        {title: "Popular", input: "https://e-hentai.org/popular", script: "gen.js"},
+		{title: "All-Time", input: "https://e-hentai.org/toplist.php?tl=11", script: "gen.js"},
+		{title: "Past Year", input: "https://e-hentai.org/toplist.php?tl=12", script: "gen.js"},
+		{title: "Past Month", input: "https://e-hentai.org/toplist.php?tl=13", script: "gen.js"},
+		{title: "Yesterday", input: "https://e-hentai.org/toplist.php?tl=15", script: "gen.js"}
     ]);
 }
