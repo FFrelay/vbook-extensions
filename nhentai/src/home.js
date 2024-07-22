c45f34eca6cd5df9b05b0debd15636871adfae36
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-07-22 12:55:39 +0700

    fix nhentai

--- nhentai/src/home.js
+++ nhentai/src/home.js
@@ -1,6 +1,6 @@
 function execute() {
     return Response.success([
-        {title: "Popular Now", input: "https://nhentai.net", script: "gen.js"}
+        {title: "Popular Now", input: "https://nhentai.net", script: "gen.js"},
 		{title: "New uploads", input: "https://nhentai.net", script: "gen2.js"}
     ]);
 }
