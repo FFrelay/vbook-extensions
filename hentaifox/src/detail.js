f9e226b953ba1a56f787f0758eeb05a1031a93af
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-07-22 15:24:00 +0700

    update

--- hentaifox/src/detail.js
+++ hentaifox/src/detail.js
@@ -6,7 +6,7 @@ function execute(url) {
         cover: info.select(".gallery_left .cover img").attr("src"),
         host: "https://hentaifox.com/",
         author: info.select(".gallery_right .artists li a").text(),
-        description: doc.select(".gallery_right .info").html(),
-        detail: info.select(".gallery_right .categories").text(),
+        detail: doc.select(".gallery_right .info").html(),
+        description: doc.select(".gallery_right .categories").html(),
     })
 }
