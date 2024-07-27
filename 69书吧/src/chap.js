8fe53a8d1a527bac872a6b9414091d628909bc50
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-07-27 08:49:38 +0700

    Fix bugs

--- 69书吧/src/chap.js
+++ 69书吧/src/chap.js
@@ -14,6 +14,7 @@ function execute(url) {
     htm = htm.html()
     htm = cleanHtml(htm)
         .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
+		.replace(/^第([零一二三四五六七八九十百千]+)章.*?<br>/, '')
         .replace('(本章完)', '')
         ;
     return Response.success(htm);
