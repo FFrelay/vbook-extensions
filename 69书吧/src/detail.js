8cb2888c7434f6008165cb6ac66a9c6c90e2ab01
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-07-27 09:17:57 +0700

    fix

--- 69书吧/src/detail.js
+++ 69书吧/src/detail.js
@@ -12,8 +12,8 @@ function execute(url) {
             name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
             cover: $.Q(doc, 'div.bookimg2 > img').attr('src'),
             author: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
-            description: $.Q(doc, 'div.content > p').html(),
-            detail: $.QA(doc, 'div.booknav2 > p', {m: x => x.text(), j: '<br>'}),
+            description: $.Q(doc, 'div.content p').html(),
+            detail: $.QA(doc, 'div.booknav2 p', {m: x => x.text(), j: '<br>'}),
             host: BASE_URL
         })
     }
