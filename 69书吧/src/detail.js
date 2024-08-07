9c8a785a3db67a8a88b930cfd94595e791a9c568
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-08-07 16:17:46 +0700

    fix

--- 69书吧/src/detail.js
+++ 69书吧/src/detail.js
@@ -7,16 +7,10 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html('gbk');
-            var m, id, cover;
-			var link = url;
-
-            if ((m = link.match(BASE_URL + /.+\/book\/(.*?)\.htm/)) && m[1] && (id = m[1])) {
-                cover = String.format('{0}/fengmian/{1}/{2}/{3}s.jpg', "https://cdn.shucdn.com", Math.floor(id / 1000), id, id);
-            }
+            
         return Response.success({
             name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
-            //cover: $.Q(doc, 'div.bookimg2 > img').attr('src'),
-			cover: cover || '',
+            cover: $.Q(doc, 'div.bookimg2 > img').attr('src'),
             author: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
             description: $.Q(doc, 'div.content > p').html(),
             detail: $.QA(doc, 'div.booknav2 p', {m: x => x.text(), j: '<br>'}),
