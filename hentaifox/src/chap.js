a3d90fe9f5d50c724a452b0260d510dddcd2119a
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-11-26 13:56:06 +0700

    fix

--- hentaifox/src/chap.js
+++ hentaifox/src/chap.js
@@ -1,22 +1,3 @@
-//function execute(url) {
-//    var doc = Http.get(url + "/").string()
-//    var totalPage = parseInt(doc.match(/load_pages.+value="(\d+)"/)[1])
-//    var listImage = []
-//    //----load page 1
-//    var newUrl = url.replace('gallery','g')
-//    var doc = Http.get(newUrl + "/1/").html()
-//    var image = doc.select("#gimg").attr("data-src")
-//    listImage.push(image)
-//    var splitUrl = image.match(/(.+)(\d+)(.jpg|.jpeg|.png|.webp)/) 
-//    
-//    //return Response.success(doc)
-//    
-//    for(var i=2 ; i<=totalPage;i++){
-//        listImage.push(splitUrl[1] + i + splitUrl[3])
-//    }
-//    return Response.success(listImage)
-//}
-
 function execute(url) {
     var doc = Http.get(url + "/").string()
     var totalPage = parseInt(doc.match(/load_pages.+value="(\d+)"/)[1])
@@ -24,7 +5,7 @@ function execute(url) {
     //----load page 1
     var newUrl = url.replace('gallery','g')
     var doc = Http.get(newUrl + "/1/").html()
-    var image = 'http:' + doc.select("#gimg").attr("src").replace('http:','')
+    var image = doc.select("#gimg").attr("data-src")
     listImage.push(image)
     var splitUrl = image.match(/(.+)(\d+)(.jpg|.jpeg|.png|.webp)/) 
     
