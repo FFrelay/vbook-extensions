838729b74243cf3b0d7782e940a919349aca595c
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-11-26 13:43:48 +0700

    fix

--- hentaifox/src/chap.js
+++ hentaifox/src/chap.js
@@ -22,33 +22,17 @@ function execute(url) {
     var totalPage = parseInt(initialDoc.match(/load_pages.+value="(\d+)"/)[1]);
     var listImage = [];
 
-    try {
-        // Load page 1
-        var newUrl = url.replace('gallery', 'g');
-        var pageDoc = Http.get(newUrl + "/1/").html();
-        var image = pageDoc.select("#gimg").attr("data-src");
-        listImage.push(image);
-        
-        var splitUrl = image.match(/(.+)(\d+)(.jpg|.jpeg|.png|.webp)/);
-
-        // Load remaining pages
+    // Load page 1
+    var newUrl = url.replace('gallery', 'g');
+    var firstPageDoc = Http.get(newUrl + "/1/").html();
+    var image = 'https:' + firstPageDoc.select("#gimg").attr("src").replace('https:', '');
+    listImage.push(image);
+    
+    var splitUrl = image.match(/(.+)(\d+)(.jpg|.jpeg|.png|.webp)/);
+    
+    if (splitUrl) {
         for (var i = 2; i <= totalPage; i++) {
             listImage.push(splitUrl[1] + i + splitUrl[3]);
-        }
-    } catch (error) {
-        console.log("Image load error", error);
-        
-        // Fallback: use .webp for splitUrl if there's an error
-        var newimage = image.replace(/\.(jpg|jpeg|png)/, '.webp');
-        var splitUrlFallback = newimage.match(/(.+)(\d+)(.webp)/);
-        
-        if (splitUrlFallback) {
-            listImage = [];
-            for (var i = 1; i <= totalPage; i++) {
-                listImage.push(splitUrlFallback[1] + i + splitUrlFallback[3]);
-            }
-        } else {
-            console.log("Fallback failed: Invalid image URL format");
         }
     }
 
