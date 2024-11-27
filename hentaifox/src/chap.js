18a7bbee48971416e217c83d9e5e76b69cdce10c
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-11-27 23:04:23 +0700

    fix

--- hentaifox/src/chap.js
+++ hentaifox/src/chap.js
@@ -1,33 +1,18 @@
 function execute(url) {
     var doc = Http.get(url + "/").string()
-	var newUrl = url.replace('gallery','g')
-	var totalPage = parseInt(doc.match(/load_pages.+value="(\d+)"/)[1])
-	var listImage = []
-	for(var i=1 ; i<=totalPage;i++){
-		var newdoc = Http.get(newUrl + "/" + i + "/").html()
-		var image = 'https:' + newdoc.select("#gimg").attr("src")
-		var newimage = image.replace('https:https:','https:')
-		var splitUrl = newimage.match(/(.+)(\d+)(.jpg|.jpeg|.png|.webp)/)
+    var totalPage = parseInt(doc.match(/load_pages.+value="(\d+)"/)[1])
+    var listImage = []
+    //----load page 1
+    var newUrl = url.replace('gallery','g')
+    var doc = Http.get(newUrl + "/1/").html()
+    var image = doc.select("#gimg").attr("data-src")
+    listImage.push(image)
+    var splitUrl = image.match(/(.+)(\d+)(.jpg|.jpeg|.png|.webp)/) 
+    
+    //return Response.success(doc)
+    
+    for(var i=2 ; i<=totalPage;i++){
         listImage.push(splitUrl[1] + i + splitUrl[3])
     }
     return Response.success(listImage)
 }
-
-//function execute(url) {
-//    var doc = Http.get(url + "/").string()
-//    var totalPage = parseInt(doc.match(/load_pages.+value="(\d+)"/)[1])
-//    var listImage = []
-//    //----load page 1
-//    var newUrl = url.replace('gallery','g')
-//    var doc = Http.get(newUrl + "/1/").html()
-//    var image = doc.select("#gimg").attr("data-src")
-//    listImage.push(image)
-//    var splitUrl = image.match(/(.+)(\d+)(.jpg|.jpeg|.png|.webp)/) 
-//    
-//    //return Response.success(doc)
-//    
-//    for(var i=2 ; i<=totalPage;i++){
-//        listImage.push(splitUrl[1] + i + splitUrl[3])
-//    }
-//    return Response.success(listImage)
-//}
