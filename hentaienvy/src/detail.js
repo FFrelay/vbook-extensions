1d8ad82d1cf6655c101ed46cac93bfaf3e620437
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-11-30 09:06:17 +0700

    fix

--- hentaienvy/src/detail.js
+++ hentaienvy/src/detail.js
@@ -1,8 +1,8 @@
 function execute(url) {
     var doc = Http.get(url + "/").html()
-    var info = doc.select(".gallery_top")
+    var info = doc.select(".gt_right")
     return Response.success({
-        name: info.select(".gt_top gt_right h1").text(),
+        name: info.select("h1").text(),
         cover: info.select(".gt_top .gt_left img").attr("src"),
         host: "https://hentaienvy.com/",
         author: info.select(".gt_right_tags > ul:nth-child(3) > span:nth-child(1) a").text(),
