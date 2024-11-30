1dc316141814f57771688c1429ac1f0bde79f50b
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-11-30 08:35:02 +0700

    fix

--- hentaienvy/src/gen.js
+++ hentaienvy/src/gen.js
@@ -1,9 +1,12 @@
 function execute(url, page) {
     if(!page) page = "1"
-    var url = "https://hentaienvy.com/?page=1"
-    else
-		url = "https://hentaienvy.com/?page=" + page
-
+    var url = "https://hentaifox.com/"
+	if(page!="1"){
+        if(page=="2")
+            url = "https://hentaifox.com/page/2/"
+        else
+            url = "https://hentaifox.com/pag/" + page + "/"
+    }
 	//var doc = Http.get(url + page + "/").html()
         
     var doc = Http.get(url).html()
