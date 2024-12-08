f184bf172f5fd1c72f8dad84fc96118e09a04a88
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-12-08 22:16:28 +0700

    update

--- hentairox/src/detail.js
+++ hentairox/src/detail.js
@@ -3,9 +3,9 @@ function execute(url) {
     var info = doc.select(".row.gallery_first")
     return Response.success({
         name: info.select(".col-md-7.col-sm-7.col-lg-8.right_details h1").text(),
-        cover: info.select(".col-md-4.col.left_cover a img").attr("src"),
+        cover: info.select(".col-md-4.col a img").attr("data-src"),
         host: "https://hentairox.com/",
-        author: info.select(".gallery_info li:nth-child(4) a:nth-child(2) span:nth-child(1)").text(),
+        author: info.select(".gallery_info li:nth-child(2) a:nth-child(2) span:nth-child(1)").text(),
         detail: doc.select(".col-md-7.col-sm-7.col-lg-8.right_details").html(),
         description: doc.select(".col-md-7.col-sm-7.col-lg-8.right_details .galleries_info a span.item_name").html(),
     })
