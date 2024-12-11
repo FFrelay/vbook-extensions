80ca49c1462c36b8d8cb93e2bb91a6549ff78998
FFrelay <1rbq6yrvn@relay.firefox.com>
2024-12-11 11:17:53 +0700

    update

--- hentaifox/src/chap.js
+++ hentaifox/src/chap.js
@@ -1,18 +1,34 @@
 function execute(url) {
-    var doc = Http.get(url + "/").string()
-    var totalPage = parseInt(doc.match(/load_pages.+value="(\d+)"/)[1])
-    var listImage = []
-    //----load page 1
+    let listImage=[]
     var newUrl = url.replace('gallery','g')
     var doc = Http.get(newUrl + "/1/").html()
     var image = doc.select("#gimg").attr("data-src")
-    listImage.push(image)
-    var splitUrl = image.match(/(.+)(\d+)(.jpg|.jpeg|.png|.webp)/) 
-    
-    //return Response.success(doc)
-    
-    for(var i=2 ; i<=totalPage;i++){
-        listImage.push(splitUrl[1] + i + splitUrl[3])
-    }
+    base_url=image.replace(/\/\d+(\.\w+)$/, '')
+doc=fetch(url)
+doc=doc.text().replace(/<!--[\s\S]*?-->/g, '')
+doc=Html.parse(doc)
+json=JSON.parse(doc.select("body>script").get(0).html().replace("var g_th = $.parseJSON('", "").replace("');", ""))
+//console.log(json.length())
+Object.entries(json).forEach(([key, value]) => {
+  let domain = ".webp";  // Mặc định là ".webp"
+  
+  switch(value[0]) {
+    case 'j':
+      domain = ".jpg";
+      break;  // Thêm break để dừng lại sau khi tìm thấy
+    case 'p':
+      domain = ".png";
+      break;
+    case 'g':
+      domain = ".gif";
+      break;
+    default:
+      domain = ".webp";  // Đây là trường hợp mặc định nếu không phải là 'j', 'p', 'g'
+  }
+
+  // Đẩy URL vào danh sách hình ảnh
+  listImage.push(base_url + "/" + key + domain);
+});
+console.log(json['2'])
     return Response.success(listImage)
 }
