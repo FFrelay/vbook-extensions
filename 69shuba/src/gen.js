37c05ee7e0697f55bdff1fe05bf226378b933b4f
FFrelay <8667267+FFrelay@users.noreply.github.com>
2026-04-17 22:12:14 +0700

    Fixed

--- 69shuba/src/gen.js
+++ 69shuba/src/gen.js
@@ -4,14 +4,18 @@ load('config.js');
 function execute(url, page) {
     page = page || '1';
     url = String.format(BASE_URL + url, page);
-    console.log(url)
-    // log(url);
+    console.log(url);
+    
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html('gbk');
         var data = [];
         var elems = $.QA(doc, 'li');
-        if (!elems.length) return Response.error(url);
+        
+        if (elems.length === 0) {
+            return Response.error("Không tìm thấy dữ liệu");
+        }
+        
         elems.forEach(function(e) {
             data.push({
                 name: $.Q(e, '.newnav h3 > a:not([class])').text().trim(),
@@ -19,10 +23,11 @@ function execute(url, page) {
                 cover: $.Q(e, 'a.imgbox > img').attr('data-src').trim(),
                 description: $.Q(e, '.ellipsis_2').text(),
                 host: BASE_URL
-            })
-        })
-        var next = parseInt(page, 10) + 1;
-        return Response.success(data, next.toString());
+            });
+        });
+        
+        let nextPage = String(parseInt(page) + 1);
+        return Response.success(data, nextPage);
     }
-    return null;
+    return Response.error("Lỗi kết nối");
 }
